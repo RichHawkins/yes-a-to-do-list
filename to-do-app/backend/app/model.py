391246cc01686e2282d32b30406b5e09a792d931
RichHawkins86 <richard@mysikabox.com>
2024-11-29 13:21:59 -0500

    feat: set up initial routes, define database models

---
+++ to-do-app/backend/app/model.py
@@ -0,0 +1,13 @@
+from . import db
+
+class User(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(80), nullable=False, unique=True)
+    email = db.Column(db.String(120), nullable=False, unique=True)
+    password = db.Column(db.String(200), nullable=False)
+
+class ToDoItem(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(200), nullable=False)
+    completed = db.Column(db.Boolean, default=False)
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'))
