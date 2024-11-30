c67d75cfc0f18e4c89edb536ca502a17ce81eed2
RichHawkins86 <richard@mysikabox.com>
2024-11-29 19:31:49 -0500

    feat: create MySQL database with User and ToDoItem tables

--- to-do-app/backend/app/__init__.py
+++ to-do-app/backend/app/__init__.py
@@ -1,20 +1,20 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
+from .config import Config
 import os
 
 db = SQLAlchemy()
 migrate = Migrate()
 
-def create_app(config_class='config.Config'):
+def create_app():
     app = Flask(__name__)
 
-    app.config.from_object(config_class)
-
+    app.config.from_object(Config)
     db.init_app(app)
     migrate.init_app(app, db)
 
-    from .routes import main
-    app.register_blueprint(main)
+    from .routes import bp
+    app.register_blueprint(bp)
 
-    return app./venv/bin/python
+    return app
--- to-do-app/backend/app/routes.py
+++ to-do-app/backend/app/routes.py
@@ -1,10 +1,9 @@
 from flask import Blueprint, render_template
-from .model import User, TodoItem
-from . import db
+from .model import ToDoItem
 
 bp = Blueprint('main', __name__)
 
 @bp.route('/')
 def index():
-    todos = TodoItem.query.all()
+    todos = ToDoItem.query.all()
     return render_template('index.html', todos=todos)
